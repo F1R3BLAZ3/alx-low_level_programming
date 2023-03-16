4e5b3a7570ae2d91be8d5adec1ca04bd3950001b
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-03-16 17:18:11 +0000

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-/** main - Prints "Programming is like building a multilingual puzzle */
+/**
+  *main - Prints "Programming is like building a multilingual puzzle
+  */
 int main(void)
 {
 	char str1[] = "\"Programming is like building a multilingual puzzle";

bfad84cb016b027837f1a87883eee774f0df2c7a
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-03-17 20:52:30 +0000

    9. Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+  * main - Entry point to program
+  * Return: End of program
+  */
+
+int main(void)
+{
+	int x = 48;
+
+	while (x < 58)
+	{
+        putchar(x);
+        if (x < 57)
+        {
+        	putchar(44);
+        	putchar(32);
+        }
+        x++;
+	}
+	putchar('\n');
+
+	return (0);
+}

8534f2e003eb1b2298840abf6db52d860b993e79
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-03-23 13:46:14 +0000

    3. The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * print_numbers - Entry point
+  *
+  */
+
+void print_numbers(void)
+{
+	char num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		_putchar(num);
+	}
+	_putchar('\n');
+}
+

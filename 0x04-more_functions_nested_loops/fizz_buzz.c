21c9ef0cd9bf350c98fd83ab2d7cb03c6ff962d3
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-03-23 20:06:26 +0000

    9. Fizz-Buzz

--- 0x04-more_functions_nested_loops/fizz_buzz.c
+++ 0x04-more_functions_nested_loops/fizz_buzz.c
@@ -16,26 +16,21 @@ int main(void)
 
 	for (num = 1; num <= 100; num++)
 	{
-		if ((num % 3 == 0) && (num % 5 == 0))
+		if (num % 3 == 0 && num % 5 == 0)
 		{
-			printf("FizzBuzz");
+			printf("FizzBuzz ");
 		}
 		else if (num % 3 == 0)
 		{
-			printf("Fizz");
+			printf("Fizz ");
 		}
 		else if (num % 5 == 0)
 		{
-			printf("Buzz");
+			printf("Buzz ");
 		}
 		else
 		{
-			printf("%d", num);
-		}
-
-		if (num != 100)
-		{
-			printf(" ");
+			printf("%d ", num);
 		}
 
 	}

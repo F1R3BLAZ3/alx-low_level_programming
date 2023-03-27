0bacf29ee54a50281206b8c891e67379dc1e381a
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-03-27 18:23:48 +0000

    4. I can only go one way. I've not got a reverse gear

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,16 +9,15 @@
  */
 void print_rev(char *s)
 {
-	int len = 0;
+	int i, len;
 
-	/* Find the length of the string */
+	len = 0;
 	while (s[len] != '\0')
 	{
 		len++;
 	}
 
-	/* Print the string in reverse order */
-	for (int i = len - 1; i >= 0; i--)
+	for (i = len - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}

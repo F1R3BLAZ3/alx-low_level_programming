74f0466c335ec999e3ed85a5388a26a5b31bab71
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-03-30 16:00:22 +0000

    5. Always look up

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
  * string_toupper - changes all lowercase letters of a string to uppercase
  * @str: the string to modify
@@ -12,7 +14,7 @@ char *string_toupper(char *str)
 	{
 		if (str[i] >= 97 && str[i] <= 122)
 		{
-			str[i] = str[i] - 32;
+			str[i] -= 32;
 		}
 		i++;
 	}

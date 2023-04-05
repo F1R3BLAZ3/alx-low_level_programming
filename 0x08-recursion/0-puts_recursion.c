c8ef146bb548898edfd5bdc9264d634121b77470
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-04-05 11:58:56 +0000

    Task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/** _puts_recursion - Prints a string, followed by a new line.
+  * @s: The string to be printed.
+  *
+  * Return: Nothing
+  */
+void _puts_recursion(char *s)
+{
+	while (*s)
+	{
+		_putchar(*s);
+		s++;
+	}
+	_putchar('\n');
+}

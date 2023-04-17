6bbc383cb636c5c1459be554f99d9e41ec2c6992
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-04-17 20:01:59 +0000

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef MYHEADER_H
+#define MYHEADER_H
+
+/**
+ * struct dog - Dog's particulars
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Owner of dag
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif

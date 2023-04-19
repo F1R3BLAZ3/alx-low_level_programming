656b1ecc71ed7c2ca4210a46058dedafc3d4440a
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-04-19 17:25:06 +0000

    Task 0

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,6 +2,7 @@
 #define FUNC_H
 
 #include <stddef.h>
+#include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));int _putchar(char c);
 void array_iterator(int *array, size_t size, void (*action)(int));

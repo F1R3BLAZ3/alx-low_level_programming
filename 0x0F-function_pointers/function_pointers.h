b59c246109893a838082ddf86f316ffbee66956f
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-04-19 16:36:35 +0000

    Task 0

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,8 @@
 #ifndef FUNC_H
 #define FUNC_H
 
+#include <stddef.h>
+
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));int _putchar(char c);
 int int_index(int *array, int size, int (*cmp)(int));

4c7463615942e662608a4c68f92a62c176bf9265
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-04-19 15:52:51 +0000

    Task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+  * print_name - Print a name.
+  * @name: Parameter.
+  * @f: Pointer to function.
+  */
+void print_name(char *name, void (*f)(char *))
+{
+	if(name == NULL || f == NULL)
+		return;
+	f(name);
+}

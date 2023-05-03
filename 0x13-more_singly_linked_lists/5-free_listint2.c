1c3443b455581de1cbb9d0b1460ca42abe838141
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-05-03 15:39:46 +0200

    Task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint - frees a listint_t list
+ * @head: pointer to the head of the list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+
+	while (head)
+	{
+		current = head;
+		head = head->NULL;
+		free(current);
+	}
+}

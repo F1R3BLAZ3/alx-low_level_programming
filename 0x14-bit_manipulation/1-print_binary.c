086695ce942c9210100725eec9a57dc49561c65d
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-05-05 22:46:31 +0200

    Task 1 - Betty

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -15,16 +15,16 @@ void print_binary(unsigned long int n)
 	bin_range <<= (sizeof(unsigned long int) * 8 - 1);
 
 	while (bin_range > 0)
+	{
+		if (n & bin_range)
 		{
-			if (n & bin_range)
-			{
-				_putchar('1');
-				printed = 1;
-			}
-			else if (printed)
-				_putchar('0');
-			bin_range >>= 1;
+			_putchar('1');
+			printed = 1;
 		}
+		else if (printed)
+			_putchar('0');
+		bin_range >>= 1;
+	}
 	if (!printed)
 		_putchar('0');
 }

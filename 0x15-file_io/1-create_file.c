9ff8db6e32c96db191a2207e3e67d63253152931
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-05-08 17:15:31 +0200

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,29 +13,29 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-    int fd, len = 0;
+	int fd, len = 0;
 
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
 
-    fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-    if (fd == -1)
-        return (-1);
+	if (fd == -1)
+		return (-1);
 
-    if (text_content != NULL)
-    {
-        while (text_content[len])
-            len++;
+	if (text_content != NULL)
+	{
+		while (text_content[len])
+			len++;
 
-        if (write(fd, text_content, len) == -1)
-        {
-            close(fd);
-            return (-1);
-        }
-    }
+		if (write(fd, text_content, len) == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
 
-    close(fd);
+	close(fd);
 
-    return (1);
+	return (1);
 }

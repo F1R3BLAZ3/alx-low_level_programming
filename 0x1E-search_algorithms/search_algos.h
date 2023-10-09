6bc984d3831293c55e44447ee18e97e30c118f36
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-10-09 14:21:33 +0000

    Task 0

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+/* Includes */
+#include <stddef.h>
+#include <stdio.h>
+
+/* Function Declarations */
+int linear_search(int *array, size_t size, int value);
+
+#endif /* SEARCH_ALGOS_H */
